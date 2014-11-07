d4123c82db0ff523b89e301d2d2ff3b644ae3e06
Julian Skinner <ju.skinner@gmail.com>
2014-11-07 00:38:15 -0500

    Add helper methods to output site_name and tagline.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def site_name
+    I18n.t('site_name')
+  end
+  helper_method :site_name
+
+  def tag_line
+    I18n.t('tag_line')
+  end
+  helper_method :tag_line
 end
